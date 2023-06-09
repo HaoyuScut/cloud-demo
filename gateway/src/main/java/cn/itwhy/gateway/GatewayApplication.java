9f1114af6f887c5b56965881182ee2266d09b71c
HaoyuScut <904264204@qq.com>
2023-06-09 15:33:03 +0800

    路由配置

---
+++ gateway/src/main/java/cn/itwhy/gateway/GatewayApplication.java
@@ -0,0 +1,17 @@
+package cn.itwhy.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 王浩宇
+ * @version 1.0
+ * @create 2023/06/09 15:03
+ */
+
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
