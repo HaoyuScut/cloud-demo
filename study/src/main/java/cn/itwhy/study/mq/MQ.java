f6983020c25612beafd1bd6699ecd5906a5d5f77
HaoyuScut <904264204@qq.com>
2023-06-13 21:35:22 +0800

    学习文件夹创建

---
+++ study/src/main/java/cn/itwhy/study/mq/MQ.java
@@ -0,0 +1,12 @@
+package cn.itwhy.study.mq;
+
+/**
+ * @author 王浩宇
+ * @version 1.0
+ * @create 2023/06/13 21:29
+ */
+public class MQ {
+    //MQ (MessageQueue)，中文是消息队列，字面来看就是存放消息的队列。
+    //也就是事件驱动架构中的Broker。
+    //RabbitMQ ActiveMQ RocketMQ Kafka
+}
---
+++ study/src/main/java/cn/itwhy/study/mq/RabbitMQ.java
@@ -0,0 +1,9 @@
+package cn.itwhy.study.mq;
+
+/**
+ * @author 王浩宇
+ * @version 1.0
+ * @create 2023/06/13 21:33
+ */
+public class RabbitMQ {
+}
